bde1c7cecbe4a3040c3937397ca0bc07678fdb52
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-03-26 12:52:34 -0300

    Funcion anonima para llamar a funcion en base a que boton se apreta

--- practicaseventosyestado/src/main.jsx
+++ practicaseventosyestado/src/main.jsx
@@ -4,18 +4,35 @@ import { useState } from 'react'
 const App = () => {
   const [contador, updateContador] = useState(0);
 
+  const handleClick = (a) => {
+    if(a.includes("right")){
 
+      updateContador(prevContador => {
+        return prevContador + 1
+      }
+    )}
+    else {
+      updateContador(prevContador => {
+        return prevContador - 1
+      }
+    )}
+  }
+
+  const handleReset = () => {
+    updateContador(0)
+  }
+
+  const esPar = contador % 2 === 0;
+  const esParPalabra = esPar ? "Es par" : "Es impar"
 
   return (
     <>
     <h1>Contador: </h1>
     <h1>{contador}</h1>
-    <button onClick={() => {
-      //updateContador(contador + 1)
-      updateContador(prevContador => {
-        return prevContador + 1
-      })
-    }}>Incrementar</button>
+    <p>{esParPalabra}</p>
+    <button onClick={() => handleClick("right")}>Incrementar</button>
+    <button onClick={handleReset}>Reset</button>
+    <button onClick={() => handleClick("left")}>Decrementar</button>
     </>
   )
 }
