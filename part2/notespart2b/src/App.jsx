37ae0eefdd464963fecaa8d57eafe6fb861e22a8
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-03-31 23:32:39 -0300

    teorico

--- part2/notespart2b/src/App.jsx
+++ part2/notespart2b/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +23,12 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
