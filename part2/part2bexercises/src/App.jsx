4f0f01de961289e2bf151321056accb0ddc4f2b3
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-03 18:59:28 -0300

    Ejercicio 2.9 finalizado

--- part2/part2bexercises/src/App.jsx
+++ part2/part2bexercises/src/App.jsx
@@ -9,13 +9,21 @@ const Person = ({name, number}) => {
 }
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
       number: '3704659186'
+    },
+    { name: 'Jose Hernandez',
+      number: '3701231231'
+    },
+    { name: 'Jose Hernandes',
+      number: '3701231232'
     }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -42,13 +50,22 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNameFilter(event.target.value)
+  }
 
   const isValidContact = (name) => persons.some(person => person.name === name);
   
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          Filter shown with  <input value={nameFilter}
+          onChange={handleFilterChange}/>
+        </div>
       <form onSubmit={addPerson}>
+      <h2>Add a new</h2>
         <div>
           name: <input value={newName}
           onChange={handleNameChange}/>
@@ -62,9 +79,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person =>
+        {
+        persons
+        .filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+        .map(person => (
           <Person key={person.name} name={person.name} number={person.number} />
-        )}
+        ))
+        }
     </div>
   )
 }
