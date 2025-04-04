ab8d9b5643acb39bc14fb3a109f428855a545633
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2025-04-04 00:27:20 -0300

    Ejercicio 2.10 finalizado

--- part2/part2bexercises/src/App.jsx
+++ part2/part2bexercises/src/App.jsx
@@ -8,8 +8,66 @@ const Person = ({name, number}) => {
   )
 }
 
-const App = () => {
+const Filter = ({nameFilter, handleFilterChange}) => {
+  
+  return (
+    <div>
+    Filter shown with  <input value={nameFilter}
+    onChange={handleFilterChange}/>
+  </div>
+  )
+}
 
+const FormPersons = ({persons, setPersons, newName, newNumber, handleNameChange, handleNumberChange, setNewName, setNewNumber}) => {
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(isValidContact(newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const person = {
+        name: newName,
+        number: newNumber
+      } 
+      setPersons(persons.concat(person));
+    }
+    setNewName("");
+    setNewNumber("");
+  }
+  
+  const isValidContact = (name) => persons.some(person => person.name === name);
+
+  return (
+  <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName}
+        onChange={handleNameChange}/>
+        <br />
+        number: <input value={newNumber}
+        onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+  </form>
+
+  )
+}
+
+const Persons = ({persons, nameFilter}) => {
+
+  return (
+      persons
+      .filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+      .map(person => (
+        <Person key={person.name} name={person.name} number={person.number} />
+      ))
+  )
+}
+
+const App = () => {
+  
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
       number: '3704659186'
@@ -25,67 +83,33 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    if(isValidContact(newName)) {
-      alert(`${newName} is already added to phonebook`)
-    } else {
-      const person = {
-        name: newName,
-        number: newNumber
-      } 
-      setPersons(persons.concat(person));
-    }
-    setNewName("");
-    setNewNumber("");
-
-  }
-
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
   const handleFilterChange = (event) => {
     console.log(event.target.value)
     setNameFilter(event.target.value)
   }
-
-  const isValidContact = (name) => persons.some(person => person.name === name);
+  
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
   
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>
-          Filter shown with  <input value={nameFilter}
-          onChange={handleFilterChange}/>
-        </div>
-      <form onSubmit={addPerson}>
+        <Filter nameFilter = {nameFilter} handleFilterChange = {handleFilterChange}/>
       <h2>Add a new</h2>
-        <div>
-          name: <input value={newName}
-          onChange={handleNameChange}/>
-          <br />
-          number: <input value={newNumber}
-          onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+        <FormPersons newName = {newName} newNumber = {newNumber}
+        setNewName = {setNewName} setNewNumber = {setNewNumber}
+        handleNameChange = {handleNameChange} handleNumberChange = {handleNumberChange}
+        persons = {persons} setPersons = {setPersons}
+        />
       <h2>Numbers</h2>
-        {
-        persons
-        .filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
-        .map(person => (
-          <Person key={person.name} name={person.name} number={person.number} />
-        ))
-        }
+        <Persons persons = {persons} nameFilter = {nameFilter}/>
     </div>
   )
 }
